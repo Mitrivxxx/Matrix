7ec25fed74adc5b6f7560b20edcf7951a644d992
Mateusz <matsma2468@gmail.com>
2025-01-10 07:58:10 +0100

    koiec

--- matrixClass.cpp
+++ matrixClass.cpp
@@ -4,7 +4,7 @@
 #include <ctime>
 
 // Default constructor
-matrixClass::matrixClass() : n(0), data(nullptr) {}
+matrixClass::matrixClass() : n(0), data(0) {}
 
 // Parameterized constructor
 matrixClass::matrixClass(int n) : n(n) {
@@ -16,6 +16,10 @@ matrixClass::matrixClass(int n) : n(n) {
 
 // Parameterized constructor with array
 matrixClass::matrixClass(int n, int* t) : n(n) {
+    if (t == nullptr) {
+        throw std::invalid_argument("Input array cannot be null");
+    }
+
     data = new int* [n];
     for (int i = 0; i < n; ++i) {
         data[i] = new int[n];
@@ -24,6 +28,7 @@ matrixClass::matrixClass(int n, int* t) : n(n) {
         }
     }
 }
+
 
 // Copy constructor
 matrixClass::matrixClass(const matrixClass& m) : n(m.n) {
@@ -119,24 +124,64 @@ matrixClass& matrixClass::losuj(int x) {
 
 // Operator implementations
 matrixClass& matrixClass::operator+(const matrixClass& m) {
-    // Implement addition logic...
-    return *this;
+    if (n != m.n) {
+        throw std::invalid_argument("Matrices must be of the same size for addition");
+    }
+
+    matrixClass* result = new matrixClass(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            result->data[i][j] = data[i][j] + m.data[i][j];
+        }
+    }
+    return *result;
 }
 
 matrixClass& matrixClass::operator*(const matrixClass& m) {
-    // Implement multiplication logic...
-    return *this;
+    if (n != m.n) {
+        throw std::invalid_argument("Matrices must be of the same size for multiplication");
+    }
+
+    matrixClass* result = new matrixClass(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            result->data[i][j] = 0;
+            for (int k = 0; k < n; ++k) {
+                result->data[i][j] += data[i][k] * m.data[k][j];
+            }
+        }
+    }
+    return *result;
 }
 
 matrixClass& matrixClass::operator+(int a) {
-    // Implement addition logic...
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            data[i][j] += a;
+        }
+    }
     return *this;
 }
 
 matrixClass& matrixClass::operator*(int a) {
-    // Implement multiplication logic...
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            data[i][j] *= a;
+        }
+    }
     return *this;
 }
+
+
+matrixClass& matrixClass::operator-(int a) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			data[i][j] -= a;
+		}
+	}
+	return *this;
+}
+
 
 // The remaining operators should be implemented similarly...
 
@@ -160,12 +205,202 @@ bool matrixClass::operator==(const matrixClass& m) const {
     return true;
 }
 
+
 bool matrixClass::operator>(const matrixClass& m) const {
-    // Implement comparison logic...
-    return false;
+    if (n != m.n) {
+        throw std::invalid_argument("Matrices must be of the same size for comparison");
+    }
+
+    int sum1 = 0, sum2 = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            sum1 += data[i][j];
+            sum2 += m.data[i][j];
+        }
+    }
+    return sum1 > sum2;
 }
 
 bool matrixClass::operator<(const matrixClass& m) const {
-    // Implement comparison logic...
-    return false;
+    if (n != m.n) {
+        throw std::invalid_argument("Matrices must be of the same size for comparison");
+    }
+
+    int sum1 = 0, sum2 = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            sum1 += data[i][j];
+            sum2 += m.data[i][j];
+        }
+    }
+    return sum1 < sum2;
 }
+
+
+
+
+
+
+matrixClass& matrixClass::operator++(int) {
+    matrixClass temp = *this; // Make a copy of the current state
+    for (int i = 0; i < n*n; ++i) {
+        data[i]++;
+    }
+    return temp;
+}
+
+// Post-decrement operator
+matrixClass& matrixClass::operator--(int) {
+    matrixClass temp = *this; // Make a copy of the current state
+    for (int i = 0; i < n * n; ++i) {
+        data[i]--;
+    }
+    return temp;
+}
+matrixClass operator*(int a, const matrixClass& m) {
+    matrixClass result(m);
+    for (int i = 0; i < result.n; ++i) {
+        for (int j = 0; j < result.n; ++j) {
+            result.data[i][j] *= a;
+        }
+    }
+    return result;
+}
+matrixClass operator-(int a, const matrixClass& m) {
+    matrixClass result(m.n);
+    for (int i = 0; i < m.n; ++i) {
+        for (int j = 0; j < m.n; ++j) {
+            result.data[i][j] = a - m.data[i][j];
+        }
+    }
+    return result;
+}
+// Ustawia wartoœci na przek¹tnej macierzy
+matrixClass& matrixClass::diagonalna(int* t) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i == j) {
+                data[i][j] = t[i];
+            }
+            else {
+                data[i][j] = 0;
+            }
+        }
+    }
+    return *this;
+}
+
+
+// Ustawia wartoœci na k-tej przek¹tnej macierzy
+matrixClass& matrixClass::diagonalna_k(int k, int* t) {
+    if (k >= 0) {
+        for (int i = 0; i < n - k; ++i) {
+            data[i][i + k] = t[i];
+        }
+    }
+    else {
+        for (int i = 0; i < n + k; ++i) {
+            data[i - k][i] = t[i];
+        }
+    }
+    return *this;
+}
+
+// Ustawia wartoœci w kolumnie x
+matrixClass& matrixClass::kolumna(int x, int* t) {
+    for (int i = 0; i < n; ++i) {
+        data[i][x] = t[i];
+    }
+    return *this;
+}
+
+// Ustawia wartoœci w wierszu y
+matrixClass& matrixClass::wiersz(int y, int* t) {
+    for (int i = 0; i < n; ++i) {
+        data[y][i] = t[i];
+    }
+    return *this;
+}
+
+// Ustawia wartoœci na przek¹tnej macierzy na 1, resztê na 0
+matrixClass& matrixClass::przekatna() {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            data[i][j] = (i == j) ? 1 : 0;
+        }
+    }
+    return *this;
+}
+
+// Ustawia wartoœci pod przek¹tn¹ macierzy na 1, resztê na 0
+matrixClass& matrixClass::pod_przekatna() {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            data[i][j] = (i > j) ? 1 : 0;
+        }
+    }
+    return *this;
+}
+
+// Ustawia wartoœci nad przek¹tn¹ macierzy na 1, resztê na 0
+matrixClass& matrixClass::nad_przekatna() {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            data[i][j] = (i < j) ? 1 : 0;
+        }
+    }
+    return *this;
+}
+
+// Ustawia wartoœci w macierzy w formie szachownicy
+matrixClass& matrixClass::szachownica() {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            data[i][j] = (i + j) % 2;
+        }
+    }
+    return *this;
+}
+
+
+matrixClass operator+(int a, const matrixClass& m) {
+    matrixClass result(m);
+    for (int i = 0; i < result.n; ++i) {
+        for (int j = 0; j < result.n; ++j) {
+            result.data[i][j] += a;
+        }
+    }
+    return result;
+}
+// Operator -= implementation
+matrixClass& matrixClass::operator-=(int a) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            data[i][j] -= a;
+        }
+    }
+    return *this;
+}
+
+// Operator *= implementation
+matrixClass& matrixClass::operator*=(int a) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            data[i][j] *= a;
+        }
+    }
+    return *this;
+}
+
+// Operator = implementation
+matrixClass& matrixClass::operator=(double a) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            data[i][j] = static_cast<int>(a); // Casting double to int
+        }
+    }
+    return *this;
+}
+
+
+
--- matrixClass.h
+++ matrixClass.h
@@ -23,34 +23,35 @@ public:
     matrixClass& dowroc();
     matrixClass& losuj();
     matrixClass& losuj(int x);
-    //matrixClass& diagonalna(int* t);
-    //matrixClass& diagonalna_k(int k, int* t);
-    //matrixClass& kolumna(int x, int* t);
-    //matrixClass& wiersz(int y, int* t);
-    //matrixClass& przekatna();
-    //matrixClass& pod_przekatna();
-    //matrixClass& nad_przekatna();
-    //matrixClass& szachownica();
+    matrixClass& diagonalna(int* t);
+    matrixClass& diagonalna_k(int k, int* t);
+    matrixClass& kolumna(int x, int* t);
+    matrixClass& wiersz(int y, int* t);
+    matrixClass& przekatna();
+    matrixClass& pod_przekatna();
+    matrixClass& nad_przekatna();
+    matrixClass& szachownica();
 
     // Operators
     matrixClass& operator+(const matrixClass& m);
     matrixClass& operator*(const matrixClass& m);
     matrixClass& operator+(int a);
     matrixClass& operator*(int a);
-    //matrixClass& operator-(int a);
-    //friend matrixClass operator+(int a, const matrixClass& m);
-    //friend matrixClass operator*(int a, const matrixClass& m);
-    //friend matrixClass operator-(int a, const matrixClass& m);
-    //matrixClass& operator++(int);
-    //matrixClass& operator--(int);
+    matrixClass& operator-(int a);
+    friend matrixClass operator+(int a, const matrixClass& m);
+    friend matrixClass operator*(int a, const matrixClass& m);
+    friend matrixClass operator-(int a, const matrixClass& m);
+    matrixClass& operator++(int);
+    matrixClass& operator--(int);
     matrixClass& operator+=(int a);
-    //matrixClass& operator-=(int a);
-    //matrixClass& operator*=(int a);
-    //matrixClass& operator=(double a); // Corrected operator
+    matrixClass& operator-=(int a);
+    matrixClass& operator*=(int a);
+    matrixClass& operator=(double a); // Corrected operator
     friend std::ostream& operator<<(std::ostream& o, const matrixClass& m);
     bool operator==(const matrixClass& m) const;
     bool operator>(const matrixClass& m) const;
     bool operator<(const matrixClass& m) const;
+
 };
 
 #endif // matrixClass_H
